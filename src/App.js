2fd1ab2377105a37bb0044b9e18b033c66c0effe
Manoj Kumar Surabhi <97968028+Manoj-kumar-surabhi@users.noreply.github.com>
2024-02-22 00:32:08 +0000

    A footer is added to home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./styles.css";
 import Nav from "./react_components/navigation.jsx";
 import Home from "./react_components/home.jsx";
+import Footer from "./react_components/footer.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
     <div className="App">
       <Nav />
       <Home />
+      <Footer />
     </div>
   );
 }

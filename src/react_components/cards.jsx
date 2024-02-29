e9c31b9f17f9cc2af03a574aade9b061df6ec2b4
Manoj Kumar Surabhi <97968028+Manoj-kumar-surabhi@users.noreply.github.com>
2024-02-29 17:33:17 +0000

    Prototype of cards has been added

---
+++ src/react_components/cards.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Card from "../prototypes/card";
+
+import image1 from "../assets/image1.jpg";
+import image2 from "../assets/image2.jpg";
+import image3 from "../assets/image3.jpg";
+
+const cards = [
+  {
+    id: 1,
+    title: "GETTING STARTED",
+    image: image1,
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "BEGGINER PROGRAMS",
+    image: image2,
+    url: "/",
+  },
+  {
+    id: 3,
+    title: "MASS GAIN PROGRAMS",
+    image: image3,
+    url: "/",
+  },
+];
+
+function Cards() {
+  return (
+    <div className="container d-flex justify-content-center align-items-center h-100">
+      <div className="row">
+        {cards.map(({ title, image, url, id }) => (
+          <div className="col-md-4" key={id}>
+            <Card imageSource={image} title={title} url={url} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
